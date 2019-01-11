4f97e0a4ddd403093b86a53d02308c497cb1ac2a
Fosso Arcel <kraulain@gmail.com>
2019-01-11 16:18:49 +0100

    add helper methods to expense model

--- lib/entities/expense.dart
+++ lib/entities/expense.dart
@@ -21,22 +21,37 @@ class Expense {
 
   int id;
   num amount;
-  DateTime dateTime;
+  int dateTimeMillis;
   String reason;
+  DateTime dateTime;
 
-  Expense({this.id = 0, this.amount, this.dateTime, this.reason});
+  Expense({this.id = 0, this.amount, this.dateTimeMillis, this.reason, this.dateTime}){
+    if(this.dateTimeMillis != null){
+      this.dateTime = convertMillisecondsToDateTime(dateTimeMillis);
+    }else if (this.dateTime != null){
+      this.dateTimeMillis = convertDateTimeToMilliseconds(dateTime);
+    }
+  }
 
   factory Expense.fromJson(Map<String, dynamic> json) => new Expense(
     id: json["id"],
     amount: json["amount"],
-    dateTime: json["date_time"],
+    dateTimeMillis: json["date_time"],
     reason: json["reason"],
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
     "amount": amount,
-    "date_time": dateTime,
+    "date_time": dateTimeMillis,
     "reason": reason,
   };
+
+  int convertDateTimeToMilliseconds(DateTime dateTime){
+    return dateTime.millisecondsSinceEpoch;
+  }
+
+  DateTime convertMillisecondsToDateTime(int millis) {
+    return new DateTime.fromMillisecondsSinceEpoch(millis);
+  }
 }
