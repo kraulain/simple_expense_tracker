dff3acd82d42ed04713205a0416456c3dbab24a1
Fosso Arcel <kraulain@gmail.com>
2019-01-11 15:31:55 +0100

    change expense entity to model

--- lib/entities/expense.dart
+++ lib/entities/expense.dart
@@ -4,6 +4,19 @@
 *
 */
 
+import 'dart:convert';
+
+Expense expenseFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Expense.fromJson(jsonData);
+}
+
+String expenseToJson(Expense data) {
+  final dyn = data.toJson();
+  return json.encode(dyn);
+}
+
+
 class Expense {
 
   int id;
@@ -12,4 +25,18 @@ class Expense {
   String reason;
 
   Expense({this.id = 0, this.amount, this.dateTime, this.reason});
+
+  factory Expense.fromJson(Map<String, dynamic> json) => new Expense(
+    id: json["id"],
+    amount: json["amount"],
+    dateTime: json["date_time"],
+    reason: json["reason"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "amount": amount,
+    "date_time": dateTime,
+    "reason": reason,
+  };
 }
