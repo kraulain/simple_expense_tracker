18bd1a31f4ff88a6ee83a9cc5db69371c71cacc5
Fosso Arcel <kraulain@gmail.com>
2019-01-17 12:37:12 +0100

    implement database connector

---
+++ lib/helpers/db_connect.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import "package:sqflite/sqflite.dart";
+import "package:path_provider/path_provider.dart";
+
+class DBConnect {
+
+  static Database _database;
+
+  DBConnect._();
+  static final DBConnect db = DBConnect._();
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "ExpenseDB.db");
+    return await openDatabase(path, version: 1, onOpen: (db) {
+    }, onCreate: (Database db, int version) async {
+      await db.execute("CREATE TABLE expense ("
+          "id INTEGER PRIMARY KEY,"
+          "amount REAL,"
+          "date INTEGER,"
+          "category TEXT,"
+          "details TEXT"
+          ")");
+    });
+  }
+
+  Future<Database> get database async {
+    if (_database != null)
+      return _database;
+
+    // if _database is null we instantiate it
+    _database = await initDB();
+    return _database;
+  }
+
+}
